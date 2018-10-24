f164c54d0cd30ddf53fc37cf1a7a070945d16baf
Adam Gaj <35027222+adgapg@users.noreply.github.com>
2018-10-24 11:39:57 +0200

    Branch32 squash (#34)
    * commit1
    
    * commit2

--- src/test/java/com/connectors/AppTest.java
+++ src/test/java/com/connectors/AppTest.java
@@ -4,10 +4,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+// some comment
 public class AppTest {
 
     @Test
     public void testApp() {
-        assertTrue( true );
+        assertTrue( false );
     }
 }
